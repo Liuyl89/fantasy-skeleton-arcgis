eeaf5cc9d89b709494c47adf2476fe33a7a82cf5
Liuyl89 <liuyl.gisuni@gmail.com>
2017-08-29 12:27:03 +0800

    fix for eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path'),
     HtmlWebpackPlugin = require('html-webpack-plugin'),
     webpack = require('webpack')
+
 module.exports = {
     entry: {
         vendor: ['./src/vendor.js'],
@@ -10,7 +11,7 @@ module.exports = {
     output: {
         filename: '[name].js',
         path: path.resolve(__dirname, 'dist'),
-        publicPath: '/fantasy-skeleton-react/'
+        publicPath: '/fantasy-skeleton-react/',
     },
     module: {
         rules: [{
@@ -21,41 +22,41 @@ module.exports = {
                 presets: [
                     'es2015',
                     'react',
-                    'stage-1'
+                    'stage-1',
                 ],
                 plugins: [
-                    'lodash'
-                ]
-            }
+                    'lodash',
+                ],
+            },
         }, {
             test: /\.css$/,
-            loader: 'style-loader!css-loader'
+            loader: 'style-loader!css-loader',
         }, {
             test: /\.scss$/,
             exclude: /node_modules/,
             loader: 'style-loader!css-loader!sass-loader',
         }, {
             test: /\.(png|gif)$/,
-            loader: 'url-loader?limit=100000'
+            loader: 'url-loader?limit=100000',
         }, {
             test: /\.jpg$/,
-            loader: 'file-loader'
+            loader: 'file-loader',
         }, {
             test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
-            loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+            loader: 'url-loader?limit=10000&mimetype=application/font-woff',
         }, {
             test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-            loader: 'url-loader?limit=10000&mimetype=application/octet-stream'
+            loader: 'url-loader?limit=10000&mimetype=application/octet-stream',
         }, {
             test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-            loader: 'file-loader'
+            loader: 'file-loader',
         }, {
             test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-            loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
+            loader: 'url-loader?limit=10000&mimetype=image/svg+xml',
         }, {
             test: /\.json$/,
-            loader: 'json-loader'
-        }]
+            loader: 'json-loader',
+        }],
     },
     plugins: [
         new HtmlWebpackPlugin({
@@ -68,15 +69,15 @@ module.exports = {
             scripts: [{
                 file: 'modernizr.min.js',
                 path: 'modernizr/',
-                version: '2.8.3'
+                version: '2.8.3',
             }, {
                 file: 'jquery.min.js',
                 path: 'jquery/',
-                version: '3.2.1'
+                version: '3.2.1',
             }, {
                 file: 'lodash.min.js',
                 path: 'lodash.js/',
-                version: '4.17.4'
+                version: '4.17.4',
             }, {
                 file: 'prop-types.min.js',
                 path: 'https://unpkg.com/prop-types/',
@@ -85,49 +86,49 @@ module.exports = {
             }, {
                 file: 'react.js',
                 path: 'react/',
-                version: '15.6.1'
+                version: '15.6.1',
             }, {
                 file: 'react-dom.js',
                 path: 'react/',
-                version: '15.6.1'
+                version: '15.6.1',
             }, {
                 file: 'react-router-dom.js',
                 path: 'react-router-dom/',
-                version: '4.1.2'
+                version: '4.1.2',
             }, {
                 file: 'js/bootstrap.min.js',
                 path: 'bootstrap/',
-                version: '3.3.7'
+                version: '3.3.7',
             }],
             links: [{
                 rel: 'stylesheet',
                 file: 'normalize.min.css',
                 path: 'normalize/',
-                version: '7.0.0'
+                version: '7.0.0',
             }, {
                 rel: 'stylesheet',
                 file: 'assets/main.css',
                 locale: true,
-                path: ''
+                path: '',
             }, {
                 rel: 'stylesheet',
                 file: 'css/bootstrap.min.css',
                 path: 'bootstrap/',
-                version: '3.3.7'
-            }]
+                version: '3.3.7',
+            }],
         }),
         new webpack.ProvidePlugin({
-            '$': 'jquery',
-            '_': 'lodash'
+            $: 'jquery',
+            _: 'lodash',
         }),
 
     ],
     externals: [{
-        'jquery': 'jQuery',
-        'lodash': '_',
-        'react': 'React',
+        jquery: 'jQuery',
+        lodash: '_',
+        react: 'React',
         'react-dom': 'ReactDOM',
         'react-router-dom': 'ReactRouterDOM',
-        'prop-types':'PropTypes',
-    }]
+        'prop-types': 'PropTypes',
+    }],
 }
